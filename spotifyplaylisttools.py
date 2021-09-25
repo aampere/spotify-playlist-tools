cf6a425907f3ecac72c533fac538843ca0be5887
aampere <priceaaronm@gmail.com>
2021-09-25 13:08:33 -0500

    Reformatted docstrings

--- spotifyplaylisttools.py
+++ spotifyplaylisttools.py
@@ -24,11 +24,13 @@ class PlaylistMaker:
         for item in self.sp.album_tracks(aid)['items']:
             albumtrackids.append(item['id'])
         return albumtrackids        
+
     def addAlbumToPlaylist(self, aid, pid):
         albumtrackids=[]
         for item in self.sp.album_tracks(aid)['items']:
             albumtrackids.append(item['id'])
         self.sp.user_playlist_add_tracks(self.username,pid,albumtrackids)
+
     def addAlbumsToPlaylist(self, alist, pid):
         added = []
         alltracks = []
@@ -38,21 +40,25 @@ class PlaylistMaker:
                 self.addAlbumToPlaylist(aid, pid)
                 time.sleep(0.1)
                 alltracks += self.getAlbumTracks(aid)
+
     def listMyPlaylists(self):  
         playlists = self.sp.user_playlists(self.username)
         for playlist in playlists['items']:
             if playlist['owner']['id'] == self.username:
                 print(playlist['name'])
+
     def deletePlaylistsStartingWith(self, start):
         playlists = self.sp.user_playlists(self.username)
         for p in playlists['items']:
             if p['name'].startswith('start'):
                 self.sp.user_playlist_unfollow(self.username,p['uri'])
     
-    #Takes an input playlist and "explodes" it.
-    #That is, for each song in the original playlist, the entire album containing that song is added to the target playlist.
-    #The result is a much larger playlist containing the full albums of the orignal playlist's songs.
     def explodePlaylist(self, playlistid, targetplaylistid):
+        '''
+        Takes an input playlist and "explodes" it.
+        That is, for each song in the original playlist, the entire album containing that song is added to the target playlist.
+        The result is a much larger playlist containing the full albums of the orignal playlist's songs.
+        '''
         playlistoriginal = self.sp.user_playlist(self.username, playlistid)
         playlistexpandedid = targetplaylistid
         tracks = playlistoriginal['tracks']
@@ -71,16 +77,18 @@ class PlaylistMaker:
         time.sleep(0.1)
         return self.sp.search(query,limit=1, market='US')
     
-    #Spotify API provides the same excellent search function available in the UI app.
-    #It is already smart and allows for a mixture of artist name, title, misspellings, etc
-    #However, with a human-created playlist, there are errors, or the creator was playing a version of the song
-    #not available on Spotify (eg, "Remastered 2005" or "Live at X"), or the artist name differs, even though it's an identical track
-    #(eg, "Bruce Springsteen and the E Street Band" or a featured artist is included/excluded, etc)
-    #My strategy for dealing with this is to start with the full-length combined artist and track from the human-created playlist.
-    #If that search turns up anything, take the first result. If it doesn't, remove words from the back of the title phrase until it does.
-    #If still nothing turns up, search the full title, but remove words from the artist phrase.
-    #If still nothing turns up, remove words from both title and artist phrases until something turns up.
     def cleverSearch(self, track, artist, i):
+        '''
+        Spotify API provides the same excellent search function available in the UI app.
+        It is already smart and allows for a mixture of artist name, title, misspellings, etc
+        However, with a human-created playlist, there are errors, or the creator was playing a version of the song
+        not available on Spotify (eg, "Remastered 2005" or "Live at X"), or the artist name differs, even though it's an identical track
+        (eg, "Bruce Springsteen and the E Street Band" or a featured artist is included/excluded, etc)
+        My strategy for dealing with this is to start with the full-length combined artist and track from the human-created playlist.
+        If that search turns up anything, take the first result. If it doesn't, remove words from the back of the title phrase until it does.
+        If still nothing turns up, search the full title, but remove words from the artist phrase.
+        If still nothing turns up, remove words from both title and artist phrases until something turns up.
+        '''
         mintitlewords = 3;
         minartistwords = 2;
         titlewords = track.split(' ');
@@ -123,9 +131,11 @@ class PlaylistMaker:
         else:
             return self.cleverSearch(track, artist, i+1)
     
-    #The API function user_playlist_add_tracks only allows you to add 100 tracks at once.
-    #This is a convenience function that can add a large list of tracks 100 at a time.
     def addManyTracksToPlaylist(self, playlisturi, largeurilist):
+        '''
+        The API function user_playlist_add_tracks only allows you to add 100 tracks at once.
+        This is a convenience function that can add a large list of tracks 100 at a time.
+        '''
         i=0
         j=100
         while i<len(largeurilist):
